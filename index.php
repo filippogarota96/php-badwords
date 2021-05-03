49f6930691ae7b2d0448389ed0ccca1f583563f7
Filippo Garota <filogarota101@gmail.com>
2021-05-03 16:20:15 +0200

    Badword sostituita con ***

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-  $nome = 'Filippo';
+  /*Creare una variabile con un paragrafo di testo.
+  Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre * */
+  $badword = $_GET["badword"];
+  $text = 'Stamattina ho visto a quel ' . $badword . ' di Asdrubale e gli ho urlato: "ciao '. $badword . ' !!!"';
+  $badword_replace = str_replace($badword, '***', $text );
+  var_dump($badword_replace);
+  
 ?>
 
 <!DOCTYPE html>
@@ -11,9 +17,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bad Words</title>
 </head>
-
+  <p><?php echo $badword_replace; ?></p>
 <body>
-  <h1>Ciao mi chiamo <?php echo $nome ?></h1>
+  
 </body>
 
 </html>
