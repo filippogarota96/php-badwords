a1641b7cf66702c60f6493eb478690a4ee8259c6
Filippo Garota <filogarota101@gmail.com>
2021-05-03 16:48:03 +0200

    Stampo a schermo la lunghezza del paragrafo

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-  /*Creare una variabile con un paragrafo di testo.
+/*Creare una variabile con un paragrafo di testo.
   Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre * */
-  $badword = $_GET["badword"];
-  $text = 'Stamattina ho visto a quel ' . $badword . ' di Asdrubale e gli ho urlato: "ciao '. $badword . ' !!!"';
-  $badword_replace = str_replace($badword, '***', $text );
-  var_dump($badword_replace);
-  
+$badword = $_GET["badword"];
+$text = 'Stamattina ho visto a quel ' . $badword . ' di Asdrubale e gli ho urlato: "ciao ' . $badword . ' !!!"';
+$badword_replace = str_replace($badword, '***', $text);
+
+
+// $prova = "   Hello World come va  ";
+// $arr_prova = trim($prova );
+// var_dump($arr_prova);
 ?>
 
 <!DOCTYPE html>
@@ -17,9 +20,11 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bad Words</title>
 </head>
-  <p><?php echo $badword_replace; ?></p>
+<p> <?php echo 'la lunghezza del paragrafo è '. strlen($text) . ' parole.' ?></p>
+<p><?php echo $badword_replace; ?></p>
+
 <body>
-  
+
 </body>
 
 </html>
